17d6449ae90c39d067dbf3b87f9e8eea5231da92
hhy <hhy@hhy>
2021-08-20 01:12:53 +0800

    1

--- README.md
+++ README.md
@@ -1,31 +1,23 @@
-# NOH-NMS: Improving Pedestrian Detection by Nearby Objects Hallucination
+# Pedestrain Detection baseline
 
 <div align="center">
   <img src="demo/architecture.png"/>
 </div>
 
-The offical implementation for the ["NOH-NMS: Improving Pedestrian Detection by Nearby Objects Hallucination"](https://arxiv.org/pdf/2007.13376.pdf) which is published in **ACM MM 2020**. 
-
-We propose Nearby Objects Hallucinator (NOH), which pinpoints the objects nearby each proposal with a Gaussian distribution, together with NOH-NMS, which dynamically eases the suppression for the space that might contain other objects with a high likelihood.
-
-This work has won the **first place** at the [CrowdHuman Challenge, 2020](http://competition.baai.ac.cn/c/34/rank/timeline/68?sourceType=public).
-
-<div align="center">
-  <img src="demo/crowdhuman-comp.png"/>
-</div>
+The Pytoch implementation for the Pedestrian detection baseline..
 
 * This repo is implemented based on [detectron2](https://github.com/facebookresearch/detectron2).
 
 ## Performance
 |    Model    | Backbone |  AP  |  Recall |  MR  | Weights |
 |-------------|----------|------|---------|------|----------------------------------------------------------------------------------------------------------------------------------------------------------------|
-| Faster RCNN | ResNet-50| 85.0 |   87.5  | 44.5 | [faster_rcnn_model_final.pth](https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBdl9rR0czS2U3dXRiMmZ6elBoTkdHUV9BcG8_ZT1mYVFwUVo/root/content) |
-|   NOH-NMS   | ResNet-50| 88.8 |   92.6  | 43.7 | [noh_nms_model_final.pth](https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBdl9rR0czS2U3dXRjSVpvQWJQYjVPMUlWMHc_ZT00RXJZQWg/root/content)     |
+| Faster RCNN | ResNet-50| 85.0 |   87.5  | 45.2 |         |
+|  Retinanet  | ResNet-50|      |         |      |         |
 
-## Prepare Datasets
+## Preparation
 Download the CrowdHuman Datasets from http://www.crowdhuman.org/, and then move them under the directory like:
 ```
-./data/crowdhuman
+./datasets/crowdhuman
 ├── annotations
 │   └── annotation_train.odgt
 │   └── annotation_val.odgt
@@ -34,23 +26,26 @@ Download the CrowdHuman Datasets from http://www.crowdhuman.org/, and then move 
 │   └── val
 ```
 
+Download the pretrained model, and then move them under the directory like:
+```
+./detectron2/ImageNetPretrained/MSRA/R-50.pkl
+```
+
 ## Installation
 ```
-  cd detectron2
+  conda create -n ped python=3.7 -y
+  conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.1 -c pytorch
+  cd root_dir
   pip install -e . 
-  #or rebuild
-  sh build.sh
+  cd dqrf
+  pip install -e .
+  pip install pycocotools, scipy, opencv-python, pandas
 ```
 
 ## Training in Command Line
 Train Faster RCNN on 8 gpus:
 ```
 python tools/train_net.py --num-gpus 8 --config-file configs/CrowdHuman/faster_rcnn_R_50_FPN_baseline_iou_0.5.yaml
-```
-
-Train NOH-NMS on 8 gpus:
-```
-python tools/train_net.py --num-gpus 8 --config-file configs/CrowdHuman/faster_rcnn_R_50_FPN_baseline_iou_0.5_noh_nms.yaml
 ```
 
 ## Quick Start
@@ -62,11 +57,5 @@ See [GETTING_STARTED.md](GETTING_STARTED.md) in detectron2
 ## Citation
 if you find this project useful for your research, please cite:
 ```
-@inproceedings{zhou2020noh,
-  title={NOH-NMS: Improving Pedestrian Detection by Nearby Objects Hallucination},
-  author={Zhou, Penghao and Zhou, Chong and Peng, Pai and Du, Junlong and Sun, Xing and Guo, Xiaowei and Huang, Feiyue},
-  booktitle={Proceedings of the 28th ACM International Conference on Multimedia},
-  pages={1967--1975},
-  year={2020}
-}
+Please waiting!
 ```
---
+++ tools/train_net_ped.py
@@ -0,0 +1,261 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+"""
+Detection Training Script.
+
+This scripts reads a given config file and runs the training or evaluation.
+It is an entry point that is made to train standard models in detectron2.
+
+In order to let one script support training of many models,
+this script contains logic that are specific to these built-in models and therefore
+may not be suitable for your own project.
+For example, your research project perhaps only needs a single "evaluator".
+
+Therefore, we recommend you to use detectron2 as an library and take
+this file as an example of how to use the library.
+You may want to write your own script with your datasets and other customizations.
+"""
+
+import logging
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+from collections import OrderedDict
+import torch
+
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, hooks, launch
+from detectron2.evaluation import (
+    CityscapesEvaluator,
+    COCOEvaluator,
+    COCOPanopticEvaluator,
+    DatasetEvaluators,
+    PascalVOCDetectionEvaluator,
+    SemSegEvaluator,
+    verify_results,
+    CrowdHumanEvaluator,
+)
+from detectron2.modeling import GeneralizedRCNNWithTTA
+from detectron2.data import (
+    build_detection_test_loader,
+    build_detection_train_loader,
+    DatasetCatalog,
+    MetadataCatalog)
+
+from dqrf import add_dqrf_config, add_dataset_path
+from dqrf.utils.dataset_mapper import DqrfDatasetMapper, CH_DqrfDatasetMapper
+from dqrf.utils.get_crowdhuman_dicts import get_crowdhuman_dicts
+
+logger = logging.getLogger("detectron2")
+
+class Trainer(DefaultTrainer):
+    """
+    We use the "DefaultTrainer" which contains pre-defined default logic for
+    standard training workflow. They may not work for you, especially if you
+    are working on a new research project. In that case you can use the cleaner
+    "SimpleTrainer", or write your own training loop. You can use
+    "tools/plain_train_net.py" as an example.
+    """
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.clip_norm_val = 0.0
+        super().__init__(cfg)
+
+    @classmethod
+    def build_optimizer(cls, cfg, model):
+        n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        logger.info(f"Number of parameters {n_parameters}")
+
+        def is_backbone(n, backbone_names):
+            out = False
+            for b in backbone_names:
+                if b in n:
+                    out = True
+                    break
+            return out
+
+        #careful DEFORMABLE DETR yields poorer performance is its FAKE FPN is trained on the same LR as Resnet
+        #Resnet parameters name is backbone.0
+        lr_backbone_names = ['backbone.0']
+
+        param_dicts = [
+            {
+                "params": [p for n, p in model.named_parameters() if
+                           not is_backbone(n, lr_backbone_names) and
+                           not (
+                                       "roi_fc1" in n or "roi_fc2" in n or "offset" in n or "sampling_locs" in n or "sampling_cens" in n or "sampling_weight" in n or "conv_offset" in n or 'learnable_fc' in n) and p.requires_grad],
+                "lr": cfg.SOLVER.BASE_LR,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if is_backbone(n, lr_backbone_names) and
+                           not (
+                                       "roi_fc1" in n or "roi_fc2" in n or "offset" in n or "sampling_locs" in n or "sampling_cens" in n or "sampling_weight" in n or "conv_offset" in n or 'learnable_fc' in n) and p.requires_grad],
+                "lr": cfg.SOLVER.BASE_LR * cfg.SOLVER.BACKBONE_MULTIPLIER,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if
+                           ("sampling_locs" in n) and p.requires_grad],
+                "lr": cfg.SOLVER.BASE_LR * cfg.SOLVER.SAMPLE_MULTIPLIER,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if
+                           ("sampling_cens" in n) and p.requires_grad],
+                "lr": cfg.SOLVER.BASE_LR * cfg.SOLVER.CENTER_MULTPLIER,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if
+                           ("sampling_weight" in n) and p.requires_grad],
+                "lr": cfg.SOLVER.BASE_LR * cfg.SOLVER.WEIGHT_MULTIPLIER,
+            },
+
+        ]
+        optimizer = torch.optim.AdamW(param_dicts, lr=cfg.SOLVER.BASE_LR,
+                                      weight_decay=cfg.SOLVER.WEIGHT_DECAY)
+        return optimizer
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        mapper = CH_DqrfDatasetMapper(cfg, True)
+        return build_detection_train_loader(cfg, mapper=mapper)
+
+    @classmethod
+    def build_test_loader(cls, cfg, dataset_name):
+        # mapper = CH_DqrfDatasetMapper(cfg, False)
+        return build_detection_test_loader(cfg, dataset_name)
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        """
+        Create evaluator(s) for a given dataset.
+        This uses the special metadata "evaluator_type" associated with each builtin dataset.
+        For your own dataset, you can simply create an evaluator manually in your
+        script and do not have to worry about the hacky if-else logic here.
+        """
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        evaluator_list = []
+        evaluator_type = MetadataCatalog.get(dataset_name).evaluator_type
+        if evaluator_type in ["sem_seg", "coco_panoptic_seg"]:
+            evaluator_list.append(
+                SemSegEvaluator(
+                    dataset_name,
+                    distributed=True,
+                    num_classes=cfg.MODEL.SEM_SEG_HEAD.NUM_CLASSES,
+                    ignore_label=cfg.MODEL.SEM_SEG_HEAD.IGNORE_VALUE,
+                    output_dir=output_folder,
+                )
+            )
+        if evaluator_type in ["coco", "coco_panoptic_seg"]:
+            evaluator_list.append(COCOEvaluator(dataset_name, cfg, True, output_folder))
+        if evaluator_type == "coco_panoptic_seg":
+            evaluator_list.append(COCOPanopticEvaluator(dataset_name, output_folder))
+        elif evaluator_type == "cityscapes":
+            assert (
+                torch.cuda.device_count() >= comm.get_rank()
+            ), "CityscapesEvaluator currently do not work with multiple machines."
+            return CityscapesEvaluator(dataset_name)
+        elif evaluator_type == "pascal_voc":
+            return PascalVOCDetectionEvaluator(dataset_name)
+        elif evaluator_type == "crowdhuman":
+            return CrowdHumanEvaluator(dataset_name, cfg, True, output_folder)
+        if len(evaluator_list) == 0:
+            raise NotImplementedError(
+                "no Evaluator for the dataset {} with the type {}".format(
+                    dataset_name, evaluator_type
+                )
+            )
+        elif len(evaluator_list) == 1:
+            return evaluator_list[0]
+        return DatasetEvaluators(evaluator_list)
+
+    @classmethod
+    def test_with_TTA(cls, cfg, model):
+        logger = logging.getLogger("detectron2.trainer")
+        # In the end of training, run an evaluation with TTA
+        # Only support some R-CNN models.
+        logger.info("Running inference with test-time augmentation ...")
+        model = GeneralizedRCNNWithTTA(cfg, model)
+        evaluators = [
+            cls.build_evaluator(
+                cfg, name, output_folder=os.path.join(cfg.OUTPUT_DIR, "inference_TTA")
+            )
+            for name in cfg.DATASETS.TEST
+        ]
+        res = cls.test(cfg, model, evaluators)
+        res = OrderedDict({k + "_TTA": v for k, v in res.items()})
+        return res
+
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+    add_dqrf_config(cfg)
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+
+    # crowdhuman dataset for detr
+    add_dataset_path(cfg)
+    ch_train = get_crowdhuman_dicts(cfg.CH_PATH.ANNOT_PATH_TRAIN, cfg.CH_PATH.IMG_PATH_TRAIN)
+    DatasetCatalog.register(cfg.DATASETS.TRAIN[0], ch_train)
+    MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).set(thing_classes=["background", "person"])
+
+    # ch_val = get_crowdhuman_dicts(cfg.CH_PATH.ANNOT_PATH_VAL, cfg.CH_PATH.IMG_PATH_VAL)
+    # DatasetCatalog.register(cfg.DATASETS.TEST[0], ch_val)
+    # MetadataCatalog.get(cfg.DATASETS.TEST[0]).set(thing_classes=["background", "person"])
+    # MetadataCatalog.get(cfg.DATASETS.TEST[0]).set(evaluator_type='crowdhuman')
+    # MetadataCatalog.get(cfg.DATASETS.TEST[0]).set(json_file=cfg.CH_PATH.ANNOT_PATH_VAL)
+    # MetadataCatalog.get(cfg.DATASETS.TEST[0]).set(gt_dir=cfg.CH_PATH.IMG_PATH_VAL)
+
+    cfg.freeze()
+    default_setup(cfg, args)
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
+
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model)
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        if cfg.TEST.AUG.ENABLED:
+            res.update(Trainer.test_with_TTA(cfg, model))
+        return res
+
+    """
+    If you'd like to do anything fancier than the standard training logic,
+    consider writing your own training loop or subclassing the trainer.
+    """
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=args.resume)
+    if cfg.TEST.AUG.ENABLED:
+        trainer.register_hooks(
+            [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
+        )
+    return trainer.train()
+
+
+if __name__ == "__main__":
+    args = default_argument_parser().parse_args()
+    args.dist_url = 'tcp://127.0.0.1:50152'
+    # args.num_gpus = 2
+    # args.config_file = 'configs/CrowdHuman/faster_rcnn_R_50_FPN_baseline_anchor_9_iou_0.5.yaml'
+    args.config_file = 'configs/CrowdHuman/dqrf_detr_baseline.yaml'
+    # args.config_file = 'configs/CrowdHuman/retinanet_R_50_FPN_baseline_iou_0.5.yaml'
+
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
